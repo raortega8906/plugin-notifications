17274a9511121826fc20f5d732d5ddf3c49e83ba
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-27 01:32:58 +0100

    delete views

--- resources/views/components/dropdown-link.blade.php
+++
@@ -1,4 +0,0 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-left text-sm leading-5 text-[#1b1b18] dark:text-[#EDEDEC] hover:bg-[#fff2f2] dark:hover:bg-[#1D0002] focus:outline-none focus:bg-[#fff2f2] dark:focus:bg-[#1D0002] transition duration-150 ease-in-out']) }}>
-    {{ $slot }}
-</a>
-
