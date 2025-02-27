2c5c7cd2f3ae7696a4fbb1bb733cc423a7d468ef
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-27 15:06:12 +0100

    add plugin mrc and request

---
+++ app/Http/Controllers/PluginController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plugin;
+use Illuminate\Http\Request;
+
+class PluginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Plugin $plugin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Plugin $plugin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Plugin $plugin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Plugin $plugin)
+    {
+        //
+    }
+}
