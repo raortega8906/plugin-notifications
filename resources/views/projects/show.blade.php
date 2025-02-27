20569b7e915a1ffb266560e5a23be7fafe9f781c
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-27 23:18:01 +0100

    add show function to project

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
-@section('title', 'Proyectos')
+@section('title', "$project->name") 
 
 @section('content')
 <div class="container mx-auto px-4 py-8">
     <div class="flex justify-between items-center mb-6">
-        <a href="{{ route('projects.create') }}" class="bg-[#35415f] hover:bg-[#5b597c] text-white py-2 px-4 rounded">
-            Nuevo Proyecto
+        <a href="" class="bg-[#35415f] hover:bg-[#5b597c] text-white py-2 px-4 rounded">
+            Nuevo Plugin
         </a>
     </div>
+</div>
 
-    @if ($projects->count() > 0)
+    {{-- @if ($plugins->count() > 0)
         <div class="bg-white dark:bg-[#161615] shadow-md rounded-lg overflow-hidden">
             <table class="min-w-full divide-y divide-[#e3e3e0] dark:divide-[#3E3E3A]">
                 <thead class="bg-[#f8f8f7] dark:bg-[#1D1D1B]">
@@ -24,21 +25,21 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white dark:bg-[#161615] divide-y divide-[#e3e3e0] dark:divide-[#3E3E3A]">
-                    @foreach ($projects as $project)
+                    @foreach ($plugins as $plugin)
                         <tr>
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <div class="text-sm font-medium text-[#1b1b18] dark:text-[#EDEDEC]">
-                                    {{ $project->name }}
+                                    Plugin
                                 </div>
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex justify-end gap-2">
-                                <a href="{{ route('projects.show', $project) }}" class="text-[#329232] dark:text-[#51b454] hover:text-[#d8c558] dark:hover:text-[#c7a750]">
+                                <a href="" class="text-[#329232] dark:text-[#51b454] hover:text-[#d8c558] dark:hover:text-[#c7a750]">
                                     Ver
                                 </a>
-                                <a href="{{ route('projects.edit', $project) }}" class="text-[#927f32] dark:text-[#b4a751] hover:text-[#d8c558] dark:hover:text-[#c7a750]">
+                                <a href="" class="text-[#927f32] dark:text-[#b4a751] hover:text-[#d8c558] dark:hover:text-[#c7a750]">
                                     Editar
                                 </a>
-                                <form action="{{ route('projects.destroy', $project) }}" method="POST">
+                                <form action="" method="POST">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="text-[#f53003] dark:text-[#FF4433] hover:text-[#d42a02] dark:hover:text-[#ff6b5b]">
@@ -46,19 +47,15 @@
                                     </button>
                                 </form>
                             </td>
-                            {{-- <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                <a href="" class="text-[#f53003] dark:text-[#FF4433] hover:text-[#d42a02] dark:hover:text-[#ff6b5b]">Editar</a>
-                            </td> --}}
                         </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
-    @else
+    @else --}}
         <div class="bg-white dark:bg-[#161615] shadow-md rounded-lg p-6 text-center">
-            <p class="text-[#706f6c] dark:text-[#A1A09A]">{{ __('No hay proyectos registrados') }}</p>
+            <p class="text-[#706f6c] dark:text-[#A1A09A]">{{ __('No hay plugins registrados') }}</p>
         </div>
-    @endif
-</div>
+    {{-- @endif --}}
 @endsection
 
