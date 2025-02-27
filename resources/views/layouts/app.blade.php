3a9a2cf1d4891d2972a6b3b78802c965dd6f074d
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-27 22:32:18 +0100

    add favicon to app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('./favicon.ico') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('./favicon.svg') }}">
+    <link rel="icon" type="image/png" href="{{ asset('./favicon.png') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
     <title>PluginGuard - Notificaciones de vulnerabilidades de plugins</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('./favicon.ico') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('./favicon.svg') }}">
+    <link rel="icon" type="image/png" href="{{ asset('./favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
