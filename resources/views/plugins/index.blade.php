e98c1cdee66b1bf20d1706620834bdabee679cab
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-28 02:18:12 +0100

    fix index of plugin monitoring

--- resources/views/plugins/index.blade.php
+++ resources/views/plugins/index.blade.php
@@ -19,6 +19,9 @@
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-[#706f6c] dark:text-[#A1A09A] uppercase tracking-wider">
                             Versión
                         </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-[#706f6c] dark:text-[#A1A09A] uppercase tracking-wider">
+                            Monitoreo
+                        </th>
                     </tr>
                 </thead>
                 <tbody class="bg-white dark:bg-[#161615] divide-y divide-[#e3e3e0] dark:divide-[#3E3E3A]">
@@ -37,6 +40,14 @@
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <div class="text-sm font-medium text-[#1b1b18] dark:text-[#EDEDEC]">
                                     {{ $plugin->version }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap flex justify-center gap-2">
+                                <div class="text-sm font-medium text-[#1b1b18] dark:text-[#EDEDEC]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-[#f53003] dark:text-[#FF4433]">
+                                        <circle cx="11" cy="11" r="8"></circle>
+                                        <path d="m21 21-4.3-4.3"></path>
+                                    </svg>
                                 </div>
                             </td>
                         </tr>
