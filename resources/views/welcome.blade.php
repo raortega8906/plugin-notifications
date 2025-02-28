3c7093b132ec5fcc6e3aee5b1b4e8d4e142e767a
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-28 19:16:40 +0100

    change welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
                         </span>
                         <span>
                             Escanea vulnerabilidades o recibe notificaciones.<br>
-                            <a href="#" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433]">
+                            <a href="{{ asset('./images/demo.webp') }}" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433]">
                                 <span>Ver demo</span>
                                 <svg width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-2.5 h-2.5">
                                     <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor" stroke-linecap="square"/>
@@ -89,6 +89,11 @@
                     <li>
                         <a href="{{ route('register') }}" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
                             Comenzar ahora
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/raortega8906/plugin-notifications" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                            GitHub
                         </a>
                     </li>
                 </ul>
