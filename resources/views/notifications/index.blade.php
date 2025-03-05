82f8a8a193728b42680f64fccb376d42087e6b41
Rafael A. Ortega <raortega8906@gmail.com>
2025-03-06 00:17:16 +0100

    refactor code for vulnerabilities

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -7,7 +7,7 @@
     
     <div class="flex justify-between items-center mb-6">
         <a href="{{ route('dashboard') }}"
-            class="inline-block px-5 py-1.5 bg-[#35415f] border-[#35415f] hover:border-[#35415f] border text-[#fff] rounded-sm text-sm leading-normal">
+            class="inline-block px-5 py-1.5 border-[#35415f] hover:border-[#35415f] border text-[#35415f] rounded-sm text-sm leading-normal">
             Ir al dashboard
         </a>
     </div>
